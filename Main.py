ca1179a5bc02009aac9f1080d039190290b764d8
URK21CS1204-VARSHI <109951981+URK21CS1204-VARSHI@users.noreply.github.com>
2022-08-01 00:01:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ class Evaluate:
     # Write your code here
 
 
-  def _init_(self, size):
+  def __init__(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       top:An integer which points to the top most element in the stack.
